bc8290007af61cb76863b0e8337fd5780d07925f
allu <aleksijaakkojuhani@gmail.com>
2019-07-14 08:58:15 +0300

    doing stuff...

--- components/Slides.js
+++ components/Slides.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 const Slides = ({ data }) => {
+  const renderLastSlide = i => {
+    if (i === data.length - 1) {
+      return (
+        <TouchableOpacity style={styles.okButtonStyle}>
+          <Text style={{color: 'white', fontSize: 20}}>
+            Ok I got it!
+          </Text>
+        </TouchableOpacity>
+      )
+    }
+  }
 
   const renderSlides = () => {
-    return data.map(slide => {
-      console.log(slide.text);
+    return data.map((slide, i) => {
       return (
-        <View key={slide.text} style={styles.slideStyle}>
+        <View
+          key={slide.text}
+          style={[styles.slideStyle, {backgroundColor: slide.color}]}
+        >
           <Text style={styles.textStyle}>
             {slide.text}
           </Text>
+          {renderLastSlide(i)}
         </View>
       );
     });
@@ -19,7 +44,7 @@ const Slides = ({ data }) => {
   return (
     <ScrollView
       horizontal
-      style={styles.viewStyle}
+      pagingEnabled
       //contentContainerStyle={{justifyContent: 'center'}}
     >
       {renderSlides()}
@@ -28,18 +53,25 @@ const Slides = ({ data }) => {
 }
 
 const styles = StyleSheet.create({
-  viewStyle: {
-    //borderWidth: 1,
-    //borderColor: 'black',
-  },
   textStyle: {
-    fontSize: 20,
-    fontWeight: 'bold'
+    fontSize: 25,
+    //fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center'
   },
   slideStyle: {
+    //justifyContent: 'center',
+    alignItems: 'center',
+    width: SCREEN_WIDTH,
+    padding: 20,
+    paddingTop: SCREEN_HEIGHT / 2 - 40
+  },
+  okButtonStyle: {
     borderWidth: 1,
-    borderColor: 'green',
-    justifyContent: 'center'
+    borderColor: 'white',
+    padding: 10,
+    marginTop: 40,
+    borderRadius: 4
   }
 })
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -3,8 +3,9 @@ import { StyleSheet, View, Text, Button } from 'react-native';
 import Slides from '../components/Slides';
 
 const SLIDE_DATA = [
-  { text: 'Welcome to job_app' },
-  { text: 'Set your location, then swipe away' },
+  { text: 'Welcome to job_app', color: '#48BEFF' },
+  { text: 'Use this to get a job goddammit', color: '#32CDD1' },
+  { text: 'Set your location, then swipe away', color: '#43C59E' },
 ];
 
 const WelcomeScreen = ({ navigation }) => {
